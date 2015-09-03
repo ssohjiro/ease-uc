a63ec66d32a1d8244daa4d1727195353f9a8b4a9
ssohjiro@gmail.com <ssohjiro@gmail.com>
2015-09-04 05:15:11 +0900

    value name

--- ease-uc.js
+++ ease-uc.js
@@ -1,5 +1,6 @@
 
 // easing functions from "Tween.js"
+// Special Thanks for ease-component
 
 (function( factory ) {
 
@@ -14,8 +15,8 @@
 
 		define(['exports'], function(exports) {
 			// Export global even in AMD case in case this script is loaded with
-			// others that may still expect a global easeComponent.
-			root.easeComponent = factory( root, exports );
+			// others that may still expect a global easeUc.
+			root.easeUc = factory( root, exports );
 		});
 
 	// Next for CommonJS.
@@ -24,142 +25,142 @@
 
 	// Finally, as a browser global.
 	} else {
-		root.easeComponent = factory( root, {} );
+		root.easeUc = factory( root, {} );
 	}
 
-}( function( root, easeComponent ) {
+}( function( root, easeUc ) {
 	
 	// Current version of the library. Keep in sync with `package.json`.
-	easeComponent.VERSION = '0.0.1';
+	easeUc.VERSION = '0.0.3';
 
-	var previousRafUC = root.easeComponent;
-	easeComponent.noConflict = function() {
-		root.easeComponent = previousRafUC;
+	var previousEaseUc  = root.easeUc;
+	easeUc.noConflict = function() {
+		root.easeUc = previousEaseUc ;
 		return this;
 	};
 
-	easeComponent.linear = function(n){
+	easeUc.linear = function(n){
 	  return n;
 	};
 
-	easeComponent.inQuad = function(n){
+	easeUc.inQuad = function(n){
 	  return n * n;
 	};
 
-	easeComponent.outQuad = function(n){
+	easeUc.outQuad = function(n){
 	  return n * (2 - n);
 	};
 
-	easeComponent.inOutQuad = function(n){
+	easeUc.inOutQuad = function(n){
 	  n *= 2;
 	  if (n < 1) return 0.5 * n * n;
 	  return - 0.5 * (--n * (n - 2) - 1);
 	};
 
-	easeComponent.inCube = function(n){
+	easeUc.inCube = function(n){
 	  return n * n * n;
 	};
 
-	easeComponent.outCube = function(n){
+	easeUc.outCube = function(n){
 	  return --n * n * n + 1;
 	};
 
-	easeComponent.inOutCube = function(n){
+	easeUc.inOutCube = function(n){
 	  n *= 2;
 	  if (n < 1) return 0.5 * n * n * n;
 	  return 0.5 * ((n -= 2 ) * n * n + 2);
 	};
 
-	easeComponent.inQuart = function(n){
+	easeUc.inQuart = function(n){
 	  return n * n * n * n;
 	};
 
-	easeComponent.outQuart = function(n){
+	easeUc.outQuart = function(n){
 	  return 1 - (--n * n * n * n);
 	};
 
-	easeComponent.inOutQuart = function(n){
+	easeUc.inOutQuart = function(n){
 	  n *= 2;
 	  if (n < 1) return 0.5 * n * n * n * n;
 	  return -0.5 * ((n -= 2) * n * n * n - 2);
 	};
 
-	easeComponent.inQuint = function(n){
+	easeUc.inQuint = function(n){
 	  return n * n * n * n * n;
 	}
 
-	easeComponent.outQuint = function(n){
+	easeUc.outQuint = function(n){
 	  return --n * n * n * n * n + 1;
 	}
 
-	easeComponent.inOutQuint = function(n){
+	easeUc.inOutQuint = function(n){
 	  n *= 2;
 	  if (n < 1) return 0.5 * n * n * n * n * n;
 	  return 0.5 * ((n -= 2) * n * n * n * n + 2);
 	};
 
-	easeComponent.inSine = function(n){
+	easeUc.inSine = function(n){
 	  return 1 - Math.cos(n * Math.PI / 2 );
 	};
 
-	easeComponent.outSine = function(n){
+	easeUc.outSine = function(n){
 	  return Math.sin(n * Math.PI / 2);
 	};
 
-	easeComponent.inOutSine = function(n){
+	easeUc.inOutSine = function(n){
 	  return .5 * (1 - Math.cos(Math.PI * n));
 	};
 
-	easeComponent.inExpo = function(n){
+	easeUc.inExpo = function(n){
 	  return 0 == n ? 0 : Math.pow(1024, n - 1);
 	};
 
-	easeComponent.outExpo = function(n){
+	easeUc.outExpo = function(n){
 	  return 1 == n ? n : 1 - Math.pow(2, -10 * n);
 	};
 
-	easeComponent.inOutExpo = function(n){
+	easeUc.inOutExpo = function(n){
 	  if (0 == n) return 0;
 	  if (1 == n) return 1;
 	  if ((n *= 2) < 1) return .5 * Math.pow(1024, n - 1);
 	  return .5 * (-Math.pow(2, -10 * (n - 1)) + 2);
 	};
 
-	easeComponent.inCirc = function(n){
+	easeUc.inCirc = function(n){
 	  return 1 - Math.sqrt(1 - n * n);
 	};
 
-	easeComponent.outCirc = function(n){
+	easeUc.outCirc = function(n){
 	  return Math.sqrt(1 - (--n * n));
 	};
 
-	easeComponent.inOutCirc = function(n){
+	easeUc.inOutCirc = function(n){
 	  n *= 2
 	  if (n < 1) return -0.5 * (Math.sqrt(1 - n * n) - 1);
 	  return 0.5 * (Math.sqrt(1 - (n -= 2) * n) + 1);
 	};
 
-	easeComponent.inBack = function(n){
+	easeUc.inBack = function(n){
 	  var s = 1.70158;
 	  return n * n * (( s + 1 ) * n - s);
 	};
 
-	easeComponent.outBack = function(n){
+	easeUc.outBack = function(n){
 	  var s = 1.70158;
 	  return --n * n * ((s + 1) * n + s) + 1;
 	};
 
-	easeComponent.inOutBack = function(n){
+	easeUc.inOutBack = function(n){
 	  var s = 1.70158 * 1.525;
 	  if ( ( n *= 2 ) < 1 ) return 0.5 * ( n * n * ( ( s + 1 ) * n - s ) );
 	  return 0.5 * ( ( n -= 2 ) * n * ( ( s + 1 ) * n + s ) + 2 );
 	};
 
-	easeComponent.inBounce = function(n){
-	  return 1 - easeComponent.outBounce(1 - n);
+	easeUc.inBounce = function(n){
+	  return 1 - easeUc.outBounce(1 - n);
 	};
 
-	easeComponent.outBounce = function(n){
+	easeUc.outBounce = function(n){
 	  if ( n < ( 1 / 2.75 ) ) {
 		return 7.5625 * n * n;
 	  } else if ( n < ( 2 / 2.75 ) ) {
@@ -171,39 +172,39 @@
 	  }
 	};
 
-	easeComponent.inOutBounce = function(n){
-	  if (n < .5) return easeComponent.inBounce(n * 2) * .5;
-	  return easeComponent.outBounce(n * 2 - 1) * .5 + .5;
+	easeUc.inOutBounce = function(n){
+	  if (n < .5) return easeUc.inBounce(n * 2) * .5;
+	  return easeUc.outBounce(n * 2 - 1) * .5 + .5;
 	};
 
 	// aliases
-	easeComponent['in-quad'] = easeComponent.inQuad;
-	easeComponent['out-quad'] = easeComponent.outQuad;
-	easeComponent['in-out-quad'] = easeComponent.inOutQuad;
-	easeComponent['in-cube'] = easeComponent.inCube;
-	easeComponent['out-cube'] = easeComponent.outCube;
-	easeComponent['in-out-cube'] = easeComponent.inOutCube;
-	easeComponent['in-quart'] = easeComponent.inQuart;
-	easeComponent['out-quart'] = easeComponent.outQuart;
-	easeComponent['in-out-quart'] = easeComponent.inOutQuart;
-	easeComponent['in-quint'] = easeComponent.inQuint;
-	easeComponent['out-quint'] = easeComponent.outQuint;
-	easeComponent['in-out-quint'] = easeComponent.inOutQuint;
-	easeComponent['in-sine'] = easeComponent.inSine;
-	easeComponent['out-sine'] = easeComponent.outSine;
-	easeComponent['in-out-sine'] = easeComponent.inOutSine;
-	easeComponent['in-expo'] = easeComponent.inExpo;
-	easeComponent['out-expo'] = easeComponent.outExpo;
-	easeComponent['in-out-expo'] = easeComponent.inOutExpo;
-	easeComponent['in-circ'] = easeComponent.inCirc;
-	easeComponent['out-circ'] = easeComponent.outCirc;
-	easeComponent['in-out-circ'] = easeComponent.inOutCirc;
-	easeComponent['in-back'] = easeComponent.inBack;
-	easeComponent['out-back'] = easeComponent.outBack;
-	easeComponent['in-out-back'] = easeComponent.inOutBack;
-	easeComponent['in-bounce'] = easeComponent.inBounce;
-	easeComponent['out-bounce'] = easeComponent.outBounce;
-	easeComponent['in-out-bounce'] = easeComponent.inOutBounce;
+	easeUc['in-quad'] = easeUc.inQuad;
+	easeUc['out-quad'] = easeUc.outQuad;
+	easeUc['in-out-quad'] = easeUc.inOutQuad;
+	easeUc['in-cube'] = easeUc.inCube;
+	easeUc['out-cube'] = easeUc.outCube;
+	easeUc['in-out-cube'] = easeUc.inOutCube;
+	easeUc['in-quart'] = easeUc.inQuart;
+	easeUc['out-quart'] = easeUc.outQuart;
+	easeUc['in-out-quart'] = easeUc.inOutQuart;
+	easeUc['in-quint'] = easeUc.inQuint;
+	easeUc['out-quint'] = easeUc.outQuint;
+	easeUc['in-out-quint'] = easeUc.inOutQuint;
+	easeUc['in-sine'] = easeUc.inSine;
+	easeUc['out-sine'] = easeUc.outSine;
+	easeUc['in-out-sine'] = easeUc.inOutSine;
+	easeUc['in-expo'] = easeUc.inExpo;
+	easeUc['out-expo'] = easeUc.outExpo;
+	easeUc['in-out-expo'] = easeUc.inOutExpo;
+	easeUc['in-circ'] = easeUc.inCirc;
+	easeUc['out-circ'] = easeUc.outCirc;
+	easeUc['in-out-circ'] = easeUc.inOutCirc;
+	easeUc['in-back'] = easeUc.inBack;
+	easeUc['out-back'] = easeUc.outBack;
+	easeUc['in-out-back'] = easeUc.inOutBack;
+	easeUc['in-bounce'] = easeUc.inBounce;
+	easeUc['out-bounce'] = easeUc.outBounce;
+	easeUc['in-out-bounce'] = easeUc.inOutBounce;
 
-	return easeComponent;
+	return easeUc;
 }));
